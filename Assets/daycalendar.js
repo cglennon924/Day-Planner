059e2fbe0b07e6cc2d1b7549e3071ce7c0cd25bd
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-05-09 19:55:19 -0400

    Added features to button and 9-5 functions

--- Assets/daycalendar.js
+++ Assets/daycalendar.js
@@ -1,61 +1,35 @@
-$(document).ready(function() {
+var currentDate = Date(Date.now())//Actual date & time
+var calendarBtn = $(".btn");
+var currentHour = new Date().getHours();//Current hour in military time
 
-    // Added Current Date and Time to Jumbotron
-    var todaysDate = moment().format("MMMM Do YYYY, h:mm a");
-    $("#currentDay").text(todaysDate);
+// Current Day
+$("#currentDay").append(currentDate)
 
-    //Variable setting clock to 24 hour time
-    var current24Hour = moment().format('H');
+//Saving data into local storage once clicked
+calendarBtn.on("click", function() {
 
-    var myPlans = JSON.parse(localStorage.getItem("savedPlans"));
-
-    if(myPlans !== null) {
-    activityArray = myPlans
-    } else {
-    activityArray= []
+    // console.log($(this))
+  console.log($(this).val());
+  var hourToSave = $(this).val();
+  localStorage.setItem("hour" + hourToSave, $("#text" + hourToSave).val().trim());
+});
+for (let i = 9; i < 17; i++) {
+    var timeSelected = localStorage.getItem("hour"+i)
+    //Time Selected
+    if(timeSelected !== null){
+        $("#text"+i).val(timeSelected)
     }
-
-    //Empties Div with Id Day Planner before for loop is run.
-    var dayPlanner = $("#dayPlanner");
-    dayPlanner.empty();
-
-
-
-    //For Loop setting variable i equal to hours day planner is avaliable.
-    for (var i = 9; i<= 17; i++) {
-
-    //Set Identifier equal to index in array
-    var identifier = i - 9
-
-    // Create Row for Each Hour In For Loop
-    rowDiv = $("<div>");
-    rowDiv.addClass("row");
-    rowDiv.attr("index", i)
-
-    // Column 1 - Display Hour of the Day on Each Row
-    timeDiv = $("<div>");
-    timeDiv.addClass("col-md-2");
-    timeSpan = $("<span>");
-    timeSpan.attr("class", "hour");
-
-    // If Statements for AM and PM Hours
-    var displayTime = 0;
-    var morningAfternoon = "";
-    if(i > 12) {
-        displayTime = i - 12;
-        morningAfternoon = "pm";
-    } else {
-        displayTime = i;
-        morningAfternoon = "am";
+    console.log(currentHour)
+    //Past hours (if it's within 9-5)
+    if(currentHour>i){
+        $("#text"+i).addClass("past")
     }
-    };
-
-    // Add Hour to Each Row
-    timeSpan.text(displayTime + morningAfternoon);
-
-    // Append Display Hour to DOM Column 1
-    rowDiv.append(timeDiv);
-    timeDiv.append(timeSpan);
-      
-
-});                                                        
+    //Present time (if within 9-5)
+    else if (parseInt(currentHour)=== i){
+        $("#text"+i).addClass("present")
+    }
+    // Future (if in the time frame)
+    else {
+        $("#text"+i).addClass("future")
+    }
+}
